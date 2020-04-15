75d37e65967285b955bbf6a87a2881b6c6adac59
Ilya Desktop <ig@pss.one>
2020-04-15 12:18:52 +0300

    add Activity for first lesson

--- app/src/main/java/com/example/oreillypractise/MainActivity.java
+++ app/src/main/java/com/example/oreillypractise/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.oreillypractise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.main_firstLesson)
+    Button firstLessonBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        firstLessonBtn.setOnClickListener(v -> {
+            Intent firstLessonIntent = new Intent(this, FirstActivity.class);
+            startActivity(firstLessonIntent);
+        });
     }
+
+
 }
